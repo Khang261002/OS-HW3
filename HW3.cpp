b7ffd60f362b4548a7ba259fe18d1a0783c2985b
Khang Duong <80239652+Khang261002@users.noreply.github.com>
2024-04-02 22:14:40 -0500

    Update HW3.cpp

--- HW3.cpp
+++ HW3.cpp
@@ -5,26 +5,26 @@
 #include <unordered_map>
 #include <iomanip>
 
-class PageFrameTable {
+class PageToFrame {
 private:
-    std::unordered_map<int, int> page_table; // Page table to map logical page numbers to physical frame numbers
+    std::unordered_map<int, int> table; // Page table to map logical page numbers to physical frame numbers
 
 public:
-    PageFrameTable() {}
+    PageToFrame() {}
 
     // Method to map a page to a frame
-    void mapPageToFrame(int page_number, int frame_number) {
-        page_table[page_number] = frame_number;
+    void mapping(int page_number, int frame_number) {
+        table[page_number] = frame_number;
     }
 
     // Method to get the frame number for a given page number
     int getFrameNumber(int page_number) {
-        return page_table[page_number];
+        return table[page_number];
     }
 };
 
-// Function to translate logical addresses to physical addresses using the page/frame table
-void translateLogicalAddress(int logical_address, PageFrameTable& page_frame_table) {
+// Translate logical addresses to physical addresses using the page/frame table
+void translateLogicalAddress(int logical_address, PageToFrame& page_frame_table) {
     int page_number = (logical_address >> 8) & 0xFF;
     int offset = logical_address & 0xFF;
     int frame_number = page_frame_table.getFrameNumber(page_number);
@@ -38,7 +38,6 @@ void translateLogicalAddress(int logical_address, PageFrameTable& page_frame_tab
 
     int physical_address = (frame_number << 8) | offset;
 
-    // Outputting translated address details
     std::cout << "Logical Address: " << std::uppercase << std::hex << std::setw(4) << std::setfill('0') << logical_address
               << " => Page Number: " << std::uppercase << std::hex << std::setw(2) << std::setfill('0') << page_number
               << ", Offset: " << std::uppercase << std::hex << std::setw(2) << std::setfill('0') << offset
@@ -46,11 +45,11 @@ void translateLogicalAddress(int logical_address, PageFrameTable& page_frame_tab
 }
 
 int main() {
-    PageFrameTable page_frame_table;
+    PageToFrame page_frame_table;
 
-    page_frame_table.mapPageToFrame(0x3A, 0x0D);
-    page_frame_table.mapPageToFrame(0xAB, 0x2B);
-    page_frame_table.mapPageToFrame(0x56, 0x15);
+    page_frame_table.mapping(0x3A, 0x0D);
+    page_frame_table.mapping(0xAB, 0x2B);
+    page_frame_table.mapping(0x56, 0x15);
 
     int logical_addresses[] = {0x3A7F, 0xABCD, 0x5678};
 
